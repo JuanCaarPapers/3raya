d57675606f4223f3faa1eae974f34e61cff2cf58
Juan Carlos Graells Pérez <juancarlosgraellsperez@MacBook-Pro-de-Juan.local>
2022-12-01 21:09:17 +0100

    el juego

--- juego.py
+++ juego.py
@@ -1,15 +1,9 @@
 
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-#METODO SALIR
-def terminar():
-    print('ahora te sales del juego xdddd')
-#METODO DEL JUEGO
-def juego():
-    print('ahora empezaría el juego')
-#METODO DEL MENU
-def menu():
+import sys
+
+def titulo():
     print(   "______ _                           _     _               _    _____" +                                    
           "\n | ___ (_)                         (_)   | |             | |  |____ |" +                                    
           "\n | |_/ /_  ___ _ ____   _____ _ __  _  __| | ___     __ _| |      / /   ___ _ __    _ __ __ _ _   _  __ _ "+
@@ -17,24 +11,30 @@ def menu():
           "\n | |_/ | |  __| | | \ V |  __| | | | | (_| | (_) | | (_| | |  .___/ / |  __| | | | | | | (_| | |_| | (_| |"+
           "\n \____/|_|\___|_| |_|\_/ \___|_| |_|_|\__,_|\___/   \__,_|_|  \____/   \___|_| |_| |_|  \__,_|\__, |\__,_|"+
           "\n                                                                                               __/ |     "+
-          "\n                                                                                               |___/     "+
-          "\n 1. Juego 2.Instrucciones 3.Salir'"
-         )
-    seleccion = input('Selecciona un apartado del menú introduciendo su número asociado \n')
+          "\n                                                                                               |___/     "
+        )
+#METODO DEL MENU
+def menu():
     try:
-        seleccion = int(seleccion)
-        if seleccion == 1:
-            juego()
-        elif seleccion == 2:
-            instrucciones()
-        elif seleccion == 3:
-            terminar()
-        else:
-            print('Por favor selecciona una opción valida \n')
-            return menu()
+        opcion = int(input('\n 1. Juego 2.Instrucciones 3.Salir\nSelecciona un apartado del menú introduciendo su número asociado \n'))
     except ValueError:
-                    print('Por favor selecciona una opción valida \n')
-                    menu()
+        print('\nPor favor selecciona una opción valida')
+        return menu()
+    else:
+        if(opcion == 1 or opcion == 2 or opcion == 3):
+            return opcion
+        else: 
+            return menu()
+
+#
+
+#METODO SALIR
+def terminar():
+    print('ahora te sales del juego xdddd')
+    sys.exit()
+
+
+
 #METODO INSTRUCCIONES
 def instrucciones():
     print('Bienvenido al tres en raya versión python, el típico juego porteado a python.' +
@@ -47,32 +47,133 @@ def instrucciones():
     try: 
         selecion_inst = int(input('introduce 1 para volver al menú principal \n'))
         if selecion_inst == 1:
+            titulo()
             menu()
         while(selecion_inst != 1):
             print('Selecciona una opción valida \n')
             instrucciones()
     except ValueError:
-                    print('por favor selecciona una opción valida \n')
-                    instrucciones()
-menu()
+        print('por favor selecciona una opción valida \n')
+        instrucciones()
 
+#SELECCION DE PIEZA
 
 def selec_pieza():
     print('Bienvenidos al tres en raya, jugador 1 selecciona una pieza:'+
           '\n Según la pieza elegida, el jugador 2 jugará con la restante.'+
-          '\n             1: X  2: O')
-    try:
-        sel_piez = int(input('Escribe 1 o 2 para elegir pieza'))
-        while(sel_piez != 1 and sel_piez != 2):
-            print('por favor selecciona una opción valida')
-            sel_piez = int(input('Escribe 1 o 2 para elegir pieza'))
-        if sel_piez == 1:
-            fig1 = 'X'
-            fig2 = 'O'
-        elif sel_piez == 2:
-            fig1 = 'O'
-            fig2 = 'X'
-    except:
-        print('selecciona una opccion valida')
-        return selec_pieza()
-selec_pieza()
+          '\n             1: X  2: O \n')
+    seleccion = int(input('introduce la pieza con la que quieres jugar, 1 o 2 \n'))
+    if seleccion == 1:
+        return 'X'
+    elif seleccion == 2: 
+        return 'O'
+
+#METODO TABLERO
+   
+def rellenar_tablero(figura):
+    fila = (input('¿Qué fila quieres rellenar?'))
+    columna = input('¿Qué columna quieres rellenar?').upper()
+    #Para la fila 1
+    if fila == '1' and columna == 'A':
+        fila1[0] = figura
+    elif fila == '1' and columna == 'B':
+        fila1[1] = figura
+    elif fila == '1' and columna == 'C':
+        fila1[2] = figura
+
+    #Para la fila 2
+    elif fila == '2' and columna == 'A':
+        fila2[0] = figura
+    elif fila == '2' and columna == 'B':
+        fila2[1] = figura
+    elif fila == '2' and columna == 'C':
+        fila2[2] = figura
+
+    #Para la fila 3 
+    elif fila == '3' and columna == 'A':
+        fila3[0] = figura
+    elif fila == '3' and columna == 'B':
+        fila3[1] = figura
+    elif fila == '3' and columna == 'C':
+        fila3[2] = figura
+
+# tablero_final = 
+# print(tablero_final)
+
+# figura = 'X'
+# rellenar_tablero()
+# tablero_final = pd.DataFrame([fila1,fila2,fila3],columns=['A','B','C'],index=['1','2','3'])
+# print(tablero_final)
+
+def evalua(figura):
+    tab = [fila1, fila2, fila3]
+    for i in range(len(tab)):
+        for j in range(len(tab[i])):
+            if tab[i][j] != figura:
+                tab[i][j] = ''
+    if figura == 'X':
+        return tab in victoria_x
+    else:
+        return tab in victoria_O
+
+def juego():
+    jug1 = selec_pieza() 
+    if jug1 == 'X':
+        jug2 = 'O'
+    else:   
+        jug2 = 'X'
+    tablero = pd.DataFrame([fila1,fila2,fila3],columns=['A','B','C'],index=['1','2','3'])
+    print(tablero)
+    print('EVALUA: ',evalua(jug1))
+    while(not evalua(jug1) and not evalua(jug2)):
+        print('FILAS: ',fila1,fila2,fila3)
+        rellenar_tablero(jug1)
+        tablero = pd.DataFrame([fila1,fila2,fila3],columns=['A','B','C'],index=['1','2','3'])
+        print(tablero)
+        rellenar_tablero(jug2)
+        tablero = pd.DataFrame([fila1,fila2,fila3],columns=['A','B','C'],index=['1','2','3'])
+        print(tablero)
+    if evalua(jug1):
+        print('ha ganao j1')
+    else:
+        print('ha ganao j2')
+
+
+#PROGRAMA PRINCIPAL 
+fila1 = ['','','']
+fila2 = ['','','']
+fila3 = ['','','']
+
+victoria_x = [
+    [['X','X','X'],['','',''],['','','']],
+    [['','',''],['X','X','X'],['','','']],
+    [['','',''],['','',''],['X','X','X']],
+    [['X','',''],['X','',''],['X','','']],
+    [['','X',''],['','X',''],['','X','']],
+    [['','','X'],['','','X'],['','','X']],
+    [['X','',''],['','X',''],['','','X']],
+    [['','','X'],['','X',''],['X','','']]]
+
+victoria_O = [
+    [['O','O','O'],['','',''],['','','']],
+    [['','',''],['O','O','O'],['','','']],
+    [['','',''],['','',''],['O','O','O']],
+    [['O','',''],['O','',''],['O','','']],
+    [['','O',''],['','O',''],['','O','']],
+    [['','','O'],['','','O'],['','','O']],
+    [['O','',''],['','O',''],['','','O']],
+    [['','','O'],['','O',''],['O','','']]]
+
+while(True):
+        titulo()
+        opcion = menu()
+        if opcion == 1:
+            juego()
+        elif opcion == 2:
+            instrucciones()
+        elif opcion == 3:
+            terminar()
+else:
+    print('Por favor selecciona una opción valida \n')
+    menu()
+    rellenar_tablero()
